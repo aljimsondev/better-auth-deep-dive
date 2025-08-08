f08b27050143bae83284e3ed4530f21d8ab918ec
aljimsondev <aljimson.megrino@gmail.com>
2025-08-08 10:40:06 +0800

    Chore | added swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,31 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT ?? 3001;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  const config = new DocumentBuilder()
+    .setTitle('Better Auth API')
+    .setDescription('A better-auth API implementation.')
+    .setVersion('1.0')
+    .addTag('better-auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  app.use(
+    '/docs',
+    apiReference({
+      content: document,
+    }),
+  );
+
+  await app.listen(PORT);
+  Logger.debug(`Documentation can be found in http://localhost:${PORT}/docs`);
 }
 bootstrap();
