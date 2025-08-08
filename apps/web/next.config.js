36f5d5c2d769c14d2c6fbd5b06c3352fc41fecf4
aljimsondev <aljimson.megrino@gmail.com>
2025-08-08 22:36:29 +0800

    Chore | added fallback for initial build error of undefined value

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    rewrites(){
-        return [{
-            source:"/api/:path*",
-            destination:`${process.env.API_URL}/api/:path*`
-        }]
-    }
+  rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL || 'http://localhost:3000'}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
