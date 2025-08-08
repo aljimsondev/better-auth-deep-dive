49241de259a45fb02f9290710c7197f6baa9a205
aljimsondev <aljimson.megrino@gmail.com>
2025-08-09 07:35:05 +0800

    Chore | use CLIENT_URL instead of hard coded value

--- apps/web/src/lib/auth/auth-client.ts
+++ apps/web/src/lib/auth/auth-client.ts
@@ -2,5 +2,5 @@ import { createAuthClient } from 'better-auth/react';
 
 export const authClient = createAuthClient({
   basePath: '/api/auth',
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.CLIENT_URL || '',
 });
