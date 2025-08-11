f89d9751acce99cdc79c836788213f4888799ae0
aljimsondev <aljimson.megrino@gmail.com>
2025-08-11 09:26:43 +0800

    Chore | added generate schema script and comment

--- apps/api/src/core/auth/auth.ts
+++ apps/api/src/core/auth/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 
+// dummy auth instance for generating schema
 export const auth = betterAuth({
   database: drizzleAdapter({}, { provider: 'pg' }),
 });
